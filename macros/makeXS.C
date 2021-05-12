4961e5891c4814c06aa834793db9c3670a0b88cc
Tingjun Yang <tjyang@fnal.gov>
2021-05-12 00:48:48 -0500

    First version of reco based cross section calculation.

--- macros/makeXS.C
+++ macros/makeXS.C
@@ -1,5 +1,7 @@
 {
 
+#include "../SliceParams.h"
+
   gStyle->SetOptStat(0);
 
   TFile *file = TFile::Open("../install/bin/mc.root");
@@ -79,33 +81,68 @@
   h_recosliceid_allevts_cuts->SetTitle("Pion Inelastic Scatterings;Reco SliceID;Events");
   h_recosliceid_allevts_cuts->Draw();
   hint->DrawCopy("same");
-  h_recosliceid_pion_cuts->SetLineColor(2);
-  h_recosliceid_pion_cuts->Draw("same hist");
+  h_recosliceid_pioninelastic_cuts->SetLineColor(2);
+  h_recosliceid_pioninelastic_cuts->Draw("same hist");
   TLegend *leg3 = new TLegend(0.5,0.6,0.8,0.9);
   leg3->SetFillStyle(0);
   leg3->AddEntry(h_recosliceid_allevts_cuts,"Selected","ple");
   leg3->AddEntry(hint, "Selected #times purity","ple");
-  leg3->AddEntry(h_recosliceid_pion_cuts,"Selected true pions","l");
+  leg3->AddEntry(h_recosliceid_pioninelastic_cuts,"Selected true pions","l");
   leg3->Draw();
 
   TCanvas *c4 = new TCanvas("c4","c4");
-  h_truesliceid_pion_uf->SetLineColor(4);
-  h_truesliceid_pion_uf->SetMarkerColor(4);
-  h_truesliceid_pion_uf->SetTitle("Pion Inelastic Scatterings; True SliceID; Events");
-  h_truesliceid_pion_uf->Draw();
+  h_truesliceid_pioninelastic_uf->SetLineColor(4);
+  h_truesliceid_pioninelastic_uf->SetMarkerColor(4);
+  h_truesliceid_pioninelastic_uf->SetTitle("Pion Inelastic Scatterings; True SliceID; Events");
+  h_truesliceid_pioninelastic_uf->Draw();
   //hint->SetLineColor(3);
   //hint->SetMarkerColor(3);
   hint->DrawCopy("same");
-  h_truesliceid_pion_all->SetLineColor(2);
-  h_truesliceid_pion_all->SetMarkerColor(2);
-  h_truesliceid_pion_all->Draw("same hist");
+  h_truesliceid_pioninelastic_all->SetLineColor(2);
+  h_truesliceid_pioninelastic_all->SetMarkerColor(2);
+  h_truesliceid_pioninelastic_all->Draw("same hist");
   TLegend *leg4 = new TLegend(0.5,0.6,0.8,0.9);
   leg4->SetFillStyle(0);
   leg4->AddEntry(hint, "Selected #times purity","ple");
-  leg4->AddEntry(h_truesliceid_pion_uf,"Unfolded pions","ple");
-  leg4->AddEntry(h_truesliceid_pion_all,"True pions","l");
+  leg4->AddEntry(h_truesliceid_pioninelastic_uf,"Unfolded pions","ple");
+  leg4->AddEntry(h_truesliceid_pioninelastic_all,"True pions","l");
   leg4->Draw();
 
+  double Ninc[nthinslices] = {0};
+  double Nint[nthinslices] = {0};
+  double err_inc[nthinslices] = {0};
+  double err_int[nthinslices] = {0};
+
+  for (int i = 0; i<nthinslices; ++i){
+    Nint[i] = h_truesliceid_pioninelastic_uf->GetBinContent(i+2);
+    err_int[i] = h_truesliceid_pioninelastic_uf->GetBinError(i+2);
+    for (int j = i; j<=nthinslices+2; ++j){
+      Ninc[i] += h_truesliceid_pion_uf->GetBinContent(j+2);
+      err_inc[i] += pow(h_truesliceid_pion_uf->GetBinError(j+2),2);
+    }
+    err_inc[i] = sqrt(err_inc[i]);
+  }
+
+  double NA=6.02214076e23;
+  double MAr=39.95; //gmol
+  double Density = 1.39; // g/cm^3
+
+  double xs[nthinslices] = {0};
+  double err_xs[nthinslices] = {0};
+  double incE[nthinslices] = {0};
+
+  TGraphErrors *gr_trueincE = (TGraphErrors*)file->Get("gr_trueincE");
+
+  for (int i = 0; i<nthinslices; ++i){
+    xs[i] = MAr/(Density*NA*thinslicewidth)*log(Ninc[i]/(Ninc[i]-Nint[i]))*1e27;
+    //err_xs[i] = MAr/(Density*NA*thinslicewidth)*1e27*sqrt(N_int[i]+pow(N_int[i],2)/N_inc[i])/N_incidents[i];
+    incE[i] = gr_trueincE->GetPointY(i);
+    std::cout<<i<<" "<<Ninc[i]<<" "<<Nint[i]<<" "<<xs[i]<<" "<<incE[i]<<std::endl;
+  }
+
+  TGraphErrors *gr_xs = new TGraphErrors(nthinslices, incE, xs, 0, err_xs);
+  TCanvas *c5 = new TCanvas("c5");
+  gr_xs->Draw();
 
 }
   
