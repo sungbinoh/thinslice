55fc187b4eb17bb4f578ffef03ae6e0a1eb095a2
Tingjun Yang <tjyang@fnal.gov>
2021-05-12 12:46:40 -0500

    update slices.pdf

--- macros/plotslices.C
+++ macros/plotslices.C
@@ -26,8 +26,31 @@
   l.SetTextAngle(90);
   l.SetTextColor(0);
   for (int i = 1; i<=h->GetNbinsX(); ++i){
-    l.DrawLatex(h->GetBinCenter(i)+2, 0.4, Form("Slice %d",i-2));
+    l.DrawLatex(h->GetBinCenter(i)+2, 0.1, Form("Slice %d",i-2));
   }
+
+  TLine l1(-10, 0.8, 105, 0.7);
+  l1.SetLineStyle(2);
+  l1.SetLineWidth(4);
+  l1.SetLineColor(kOrange);
+  l1.Draw();
+
+  TLine l2(105, 0.7, 155, 0.9);
+  l2.SetLineStyle(2);
+  l2.SetLineWidth(4);
+  l2.SetLineColor(kOrange);
+  l2.Draw();
+
+  TLine l3(105, 0.7, 125, 0.5);
+  l3.SetLineStyle(2);
+  l3.SetLineWidth(4);
+  l3.SetLineColor(kOrange);
+  l3.Draw();
+
+  TMarker m(105, 0.7, 29);
+  m.SetMarkerSize(2);
+  m.SetMarkerColor(kOrange);
+  m.Draw();
 
   c1->Print("plots/slices.pdf");
 }
